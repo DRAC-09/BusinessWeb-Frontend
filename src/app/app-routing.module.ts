771edb2b657422acfd4fe79adad18ebe53b16213
DRAC-09 <allan.estrada9@gmail.com>
2022-07-17 17:04:53 -0600

    Añadiendo componentes de REGISTERO y LOGIN

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Importando Componentes
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent} from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+// Rutas de Navegacion
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
